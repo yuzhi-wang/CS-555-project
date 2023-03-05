684387582882e70f3d4e2ad39e2c79a8cf837cce
Kevin dsa <67777018+kevin46dsa@users.noreply.github.com>
2023-03-04 20:50:44 -0500

    init

---
+++ client/src/Components/Homepage/Homepage.js
@@ -0,0 +1,30 @@
+import React from 'react';
+/*
+import BackgroundCSL from './Carousel';
+import { getDocs } from 'firebase/firestore';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Link , useNavigate} from 'react-router-dom';
+import ListingItem from '../Listingitem/Listingitem.jsx';
+//import Slider from "../components/Slider";
+import { db } from '../../firebase';
+import queries from '../../query';
+import '../Style/style.css';
+import arrow from "../../Assets/right-arrow.png"
+
+*/
+
+const Homepage = () => {
+	
+
+	return (
+		<>
+			
+            <h1>HomePage</h1>
+            
+
+		</>
+	);
+};
+
+export default Homepage;

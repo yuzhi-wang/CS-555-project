d72197a0bc08800ecbf0fb01626bf79d0eb75162
Kevin dsa <67777018+kevin46dsa@users.noreply.github.com>
2023-03-11 14:39:37 -0500

    customer message display

---
+++ client/src/Components/Messaging/CustomerMessaging.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import { getAuth, updateProfile } from "firebase/auth";
+import DisplayMessage from './DisplayMessage';
+import { doc, getDoc  } from "firebase/firestore"; 
+import { db } from "../../firebase";
+
+function CustomerMessaging() {
+    const auth = getAuth();
+    
+  const [messages, setMessages] = useState([]);
+
+  function handleNewMessageChange(event) {
+    /*
+    setNewMessage(event.target.value);
+    */
+  }
+  let text = "test"
+
+  function handleSendMessage() {
+    /*
+    if (newMessage.trim() !== '') {
+      setMessages([...messages, newMessage]);
+      setNewMessage('');
+    }
+    */
+  } 
+  useEffect(() => {
+    async function fetchMessages() {
+      const docRef = doc(db, "CustomerSalesMessaging", auth.currentUser.uid);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        let {Messages} = docSnap.data()
+        setMessages(Messages);
+      }
+    }
+    fetchMessages();
+  }, []);
+
+
+  return (
+    <div>
+      <h2>Messages:</h2>
+      <DisplayMessage messages={messages}/>
+      <div>
+        <input type="text" value={text} onChange={handleNewMessageChange} />
+        <button onClick={handleSendMessage}>Send</button>
+      </div>
+    </div>
+  );
+}
+
+export default CustomerMessaging;
+
+

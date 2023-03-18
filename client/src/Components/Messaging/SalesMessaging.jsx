bcd655e15f005b3a3e66efb9548911fefd3bace3
Kevin dsa <67777018+kevin46dsa@users.noreply.github.com>
2023-03-18 15:12:13 -0400

    customer and sales messaging done

--- client/src/Components/Messaging/SalesMessaging.jsx
+++ client/src/Components/Messaging/SalesMessaging.jsx
@@ -1,32 +1,47 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, } from 'react';
 import { getAuth, updateProfile } from "firebase/auth";
 import DisplayMessage from './DisplayMessage';
-import { doc, getDoc  } from "firebase/firestore"; 
+import { doc, getDoc ,arrayUnion, updateDoc, arrayRemove ,serverTimestamp} from "firebase/firestore"; 
 import { db } from "../../firebase";
+import {useParams } from 'react-router-dom';
 
 function SalesMessaging() {
     const auth = getAuth();
+    const params = useParams();
     
   const [messages, setMessages] = useState([]);
+  const [salesmessage, setSalesMessage] = useState("");
 
   function handleNewMessageChange(event) {
-    /*
-    setNewMessage(event.target.value);
-    */
+    setSalesMessage(event.target.value);
   }
-  let text = "test"
+  
 
-  function handleSendMessage() {
-    /*
-    if (newMessage.trim() !== '') {
-      setMessages([...messages, newMessage]);
-      setNewMessage('');
+  async function handleSendMessage() {
+    if (salesmessage.trim() !== '') {
+      // send data to firebase
+      const messageRef = doc(db, "CustomerSalesMessaging", params.id);
+
+      
+      await updateDoc(messageRef, {
+      Messages: arrayUnion({
+        Text: salesmessage,
+        role: "sales",
+        timestamp: new Date(),
+        uid: auth.currentUser.uid,
+      })
+      });
+      
+
+
+      setSalesMessage('');
+      // fetch data from firebase
     }
-    */
   } 
   useEffect(() => {
     async function fetchMessages() {
-      const docRef = doc(db, "CustomerSalesMessaging", auth.currentUser.uid);
+      console.log('Sales messaging use effect fired')
+      const docRef = doc(db, "CustomerSalesMessaging", params.id);
       const docSnap = await getDoc(docRef);
       if (docSnap.exists()) {
         let {Messages} = docSnap.data()
@@ -39,10 +54,10 @@ function SalesMessaging() {
 
   return (
     <div>
-      <h2>Messages:</h2>
+      <h2>Sales Messages:</h2>
       <DisplayMessage messages={messages}/>
       <div>
-        <input type="text" value={text} onChange={handleNewMessageChange} />
+        <input type="text" value={salesmessage} onChange={handleNewMessageChange} />
         <button onClick={handleSendMessage}>Send</button>
       </div>
     </div>

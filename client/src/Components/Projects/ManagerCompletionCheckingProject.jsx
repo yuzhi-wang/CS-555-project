83b9abe58cc472f7665838bd43b0024ff982f511
Kevin dsa <67777018+kevin46dsa@users.noreply.github.com>
2023-04-24 22:32:48 -0400

    mamager signature after complete

--- client/src/Components/Projects/ManagerCompletionCheckingProject.jsx
+++ client/src/Components/Projects/ManagerCompletionCheckingProject.jsx
@@ -1,14 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect , useRef} from 'react';
 import { getAuth, updateProfile } from "firebase/auth";
 import { Link, useNavigate } from "react-router-dom";
-
+import SignaturePad from "react-signature-canvas"
+import "./sign.css"
+import {Popup} from "reactjs-popup"
+import 'reactjs-popup/dist/index.css';
 import { doc, getDoc ,arrayUnion, updateDoc, arrayRemove, setDoc,collection, addDoc,query, where,getDocs, deleteDoc} from "firebase/firestore"; 
 import { db } from "../../firebase";
 
 function ManagerCompletionCheckingProject() {
-
+  const sigCanvas = useRef({})
   const auth = getAuth();
-  
+  const navigate = useNavigate();
+  const [signature , setSignature] = useState(null)
   const [idetail1, setIdetail1] = useState([])
   const [idetail2, setIdetail2] = useState([])
   const [idetail3, setIdetail3] = useState([])
@@ -84,11 +88,51 @@ function ManagerCompletionCheckingProject() {
     console.log(idetail3[0])}
     
   },[idetail3])
+  //clear signature pad
+  const clear = () => sigCanvas.current.clear()
 
+  // Save Signature
+  const save = () =>{
+    setSignature(sigCanvas.current.getTrimmedCanvas().toDataURL("image/png"));
+    
+  }
+ 
   const ApproveButton = (projectId, ticketId) => {
     
-    return <button key={projectId} onClick={() => handleApprove(projectId, ticketId)}>Approve</button>
-  }
+    return (
+      <>
+    <div>
+    <h2>Sign To Mark Project Complete</h2>
+    <Popup modal trigger={<button>Open Signature Pad</button>} closeOnDocumentClick={false}>
+        {close =>(
+            <>
+                <SignaturePad ref={sigCanvas} canvasProps={{
+                    className:"signatureCanvas"
+                }} />
+                <button onClick={save}>Save</button>
+                <button onClick={clear}>Clear</button>
+                <button onClick={close}>Close</button>
+            </>
+        )}
+        </Popup>
+    <br/>
+    <br/>
+{signature ? (
+        <div>
+        <img src={signature} alt='signature' style={{display:"block",
+    margin: "0 auto",
+border:"1px solid black",
+width:"150px"}}/>
+<p>Once Signed, using Sign Pad click "Sign Contract" to Submit it</p>
+<button key={projectId} onClick={() => handleApprove(projectId, ticketId)}>Sign Contract</button>
+</div>
+) :null}
+
+</div>
+</>
+  )}
+
+
   const DisapproveButton = (projectId, ticketId) => {
     
     return <button key={ticketId} onClick={() => handleDisapprove(projectId, ticketId)}>Disapprove</button>
@@ -106,11 +150,11 @@ function ManagerCompletionCheckingProject() {
             manager_disapproval: ""
           });
         await updateDoc(infoRef1, {
-           
-            
-            Status: "Completion approved by Manager"
-
+            Status: "Completion approved by Manager",
+            managersignatureaftercomplete: signature, 
           });
+
+  
       alert(`Project ${projectId} completion approved.`);
     //   setCurrentTicket("");
     //   setCurrentProject("");
@@ -168,20 +212,18 @@ function ManagerCompletionCheckingProject() {
             <div className='tickets'>
                <ul>
                   <h4>{info.id1}</h4>
-                
-                  <div>
-                    <li>Ticket Id:{info.data1.ticketid}</li>
-                    <li>Ticket Type:{info.data1.type}</li>
-                    <li>Groundteam ID:{info.data1.groundteamid}</li>
-                    <li>Completion Description:{info.data1.completion_description}</li>
-                    <li>Completion Photo:</li>
-                    {info.data1.groundteam_img.map((imgUrl, index) => (
+                  {info.data1.groundteam_img.map((imgUrl, index) => (
                     <img 
                       key={index} 
                       src={imgUrl} 
                       style={{ maxWidth: '30%', maxHeight: '30%' }} 
                     />
                     ))}
+                  <div>
+                    <li>Ticket Id:{info.data1.ticketid}</li>
+                    <li>Ticket Type:{info.data1.type}</li>
+                    <li>Groundteam ID:{info.data1.groundteamid}</li>
+                    <li>Completion Description:{info.data1.completion_description}</li>
                   </div>
                 
                   
